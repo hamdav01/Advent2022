f965e5b8ad6c18be0c3032d2028d44a8b4bfe528
Hampus Davidsson <hampus.davidsson@gmail.com>
2022-12-04 13:14:33 +0100

    cleaner solution

--- Day4/task2.js
+++ Day4/task2.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-const between = (x, min, max) => x >= min && x <= max;
 try {
   const data = fs.readFileSync('input.txt', 'utf8');
   const lines = data.split(/\r?\n/);
@@ -8,19 +7,10 @@ try {
     const [elf1, elf2] = line.split(',');
     const [elf1Min, elf1Max] = elf1.split('-').map(Number);
     const [elf2Min, elf2Max] = elf2.split('-').map(Number);
-    if (between(elf2Min, elf1Min, elf1Max)) {
-      return true;
+    if (elf2Min > elf1Max || elf1Min > elf2Max) {
+      return false;
     }
-    if (between(elf2Max, elf1Min, elf1Max)) {
-      return true;
-    }
-    if (between(elf1Min, elf2Min, elf2Max)) {
-      return true;
-    }
-    if (between(elf1Max, elf2Min, elf2Max)) {
-      return true;
-    }
-    return false;
+    return true;
   });
   console.log('overlaps: ', overlaps.length);
 } catch (err) {
